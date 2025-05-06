9a616945bddc51ae96404285dce1f889818d2aad
nicoargiolas <argiolas.nicola00@gmail.com>
2025-05-06 14:28:44 +0200

    Snack 2

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ const somma3 = (n1, n2) => {
     return sum;
 }
 console.log(somma3(1, 2));
+
+// Snack 2
+// Crea una arrow function che calcola il quadrato di un numero.
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+const quadrato = (n) => n * n;
+console.log(quadrato(2));
