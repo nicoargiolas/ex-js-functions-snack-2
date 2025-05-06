903cfee0435a1e9ce58555f42ed578a991ad1d13
nicoargiolas <argiolas.nicola00@gmail.com>
2025-05-06 15:05:06 +0200

    Snack 8

--- script.js
+++ script.js
@@ -120,3 +120,23 @@ const eseguiEferma = (messaggio, interval, stop) => {
 }
 
 eseguiEferma("ciao", 1000, 10000);
+
+
+// Snack 8 (Bonus)
+// Crea una funzione che simula un conto alla rovescia
+// Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero. Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.
+
+const contoAllaRovescia = (n) => {
+    let count = n;
+    const intervallo = setInterval(() => {
+        console.log(n);
+        n--;
+
+        if (n === 0) {
+            console.log("Tempo scaduto!");
+            clearInterval(intervallo);
+        }
+    }, 1000);
+}
+
+contoAllaRovescia(10);
